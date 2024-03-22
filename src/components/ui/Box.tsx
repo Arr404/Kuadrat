e533d73cb597eb55a50c217829133a1707063e2e
Muhamad Fihris Aldama <fihrisaldama015@gmail.com>
2024-03-22 15:52:06 +0700

    [MOD] box size by type

--- src/components/ui/Box.tsx
+++ src/components/ui/Box.tsx
@@ -1,28 +1,37 @@
-import type { CSSProperties, FC } from 'react'
-import { memo } from 'react'
+import type { CSSProperties, FC } from "react";
+import { memo } from "react";
 
 const styles: CSSProperties = {
-    border: '1px dashed gray',
-    padding: '0.5rem 1rem',
-    cursor: 'move',
-}
+  border: "1px dashed gray",
+  padding: "0.5rem 1rem",
+  cursor: "move",
+};
 
 export interface BoxProps {
-    title: string
-    yellow?: boolean
-    preview?: boolean
+  title: string;
+  yellow?: boolean;
+  preview?: boolean;
 }
 
 export const Box: FC<BoxProps> = memo(function Box({ title, yellow, preview }) {
-    const backgroundColor = yellow ? 'yellow' : 'white'
-    const size = title === "X" ? 2 : 4
-    return (
-        <div
-            className={`p-10 w-[${(size).toString()}rem] h-[${(size).toString()}rem] flex justify-center items-center overflow-hidden p-2 cursor-move`}
-            style={{ ...styles, backgroundColor }}
-            role={preview ? 'BoxPreview' : 'Box'}
-        >
-            {title}
-        </div>
-    )
-})
+  const backgroundColor = yellow ? "yellow" : "white";
+  const size = title === "X^2" ? 4 : title == "X(h)" ? 4 : title == "X" ? 2 : 2;
+  const sizeHeight =
+    title === "X^2" ? 4 : title == "X(h)" ? 2 : title === "X" ? 4 : 2;
+  const widthFromSize = `${size}rem`;
+  const heightFromSize = `${sizeHeight}rem`;
+  return (
+    <div
+      className={`p-2 flex justify-center items-center overflow-hidden cursor-move`}
+      style={{
+        ...styles,
+        backgroundColor,
+        width: widthFromSize,
+        height: heightFromSize,
+      }}
+      role={preview ? "BoxPreview" : "Box"}
+    >
+      {title}
+    </div>
+  );
+});
